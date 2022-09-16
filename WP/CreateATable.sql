52eff7e73abe44fe6e105650205b459071cc347a
Jeyssson <61421639+jeyssson4k@users.noreply.github.com>
2022-09-16 12:34:27 -0500

    Create CreateATable.sql

---
+++ WP/CreateATable.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Persona(
+	identificacion INT NOT NULL PRIMARY KEY, 
+	nombre VARCHAR(50) NOT NULL,
+	apellido VARCHAR(50) NOT NULL,
+	correo TEXT NOT NULL,
+)
+
+CREATE TABLE Usuario(
+	identificacion INT NOT NULL,
+	usuario VARCHAR(50) NOT NULL PRIMARY KEY,
+	password VARCHAR(MAX) NOT NULL,
+
+	FOREIGN KEY (identificacion) REFERENCES Persona(identificacion),
+)
+
+CREATE TABLE Log_sesion(
+	consecutivo INT IDENTITY (1,1),
+	usuario VARCHAR(50) NOT NULL,
+	nombre_sesion VARCHAR(50) NOT NULL,
+	fecha DATETIME NOT NULL,
+)
+
+CREATE TABLE Log_database(
+	consecutivo INT IDENTITY (1,1),
+	nombre_tabla VARCHAR(50),
+	accion VARCHAR(50),
+	descripcion VARCHAR(50),
+	fecha DATETIME NOT NULL
+)
